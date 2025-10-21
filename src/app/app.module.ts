79d92d53d9f7897eab15de45b0cdc6e127362a5b
Shay Pinhas <shayphs@gmail.com>
2025-10-21 08:16:12 +0300

    devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppComponent } from './app.component';
 import { UserListComponent } from './components/user-list.component';
@@ -9,12 +10,26 @@ import { userReducer } from './store/user.reducer';
 import { UserEffects } from './store/user.effects';
 
 @NgModule({
-  declarations: [AppComponent, UserListComponent],
+  declarations: [
+    AppComponent,
+    UserListComponent // קומפוננטת רשימת משתמשים
+  ],
   imports: [
     BrowserModule,
+
+    // התחלת ה-Store עם ה-Reducer שלך
     StoreModule.forRoot({ users: userReducer }),
-    EffectsModule.forRoot([UserEffects])
+
+    // התחלת ה-Effects
+    EffectsModule.forRoot([UserEffects]),
+
+    // Redux DevTools – אפשר לבדוק את הסטור בדפדפן
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // שומר 25 מצבים אחרונים
+      logOnly: false, // אפשר גם לכתוב actions ב-devtools
+    }),
   ],
-  bootstrap: [AppComponent]
+  providers: [],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
