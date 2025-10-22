747d35e8ae5b3fe7a0eace1f566cf9bf030700be
Shay Pinhas <shayphs@gmail.com>
2025-10-22 10:02:12 +0300

    refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-import { UserOrdersComponent } from './components/user-orders/user-orders.component';
+import { UserOrdersComponent } from './pages/user-orders/user-orders.component';
 import { ordersReducer } from './store/order/order.reducer';
 import { UserEffects } from './store/users/user.effects';
 import { userReducer } from './store/users/user.reducer';
--- src/app/shared/loader/loader.component.ts
+++ src/app/shared/loader/loader.component.ts
@@ -2,9 +2,13 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-loader',
-  templateUrl: './loader.component.html',
+  template: `
+    <div class="loader-wrapper">
+      <div class="loader"></div>
+    </div>
+  `,
   styleUrls: ['./loader.component.css'],
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LoaderComponent {}
+export class LoaderComponent { }
